3dabb9a42549186a328e7442ddd71b6d5ba02a76
scrolis <scrolis@mail.ru>
2022-05-02 17:17:43 +0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    client: {
+      overlay: true,
+    },
+    open: true,
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './public/index.html',
+  }), new MiniCssExtractPlugin()],
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
