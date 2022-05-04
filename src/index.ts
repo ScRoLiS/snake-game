ea666e927e96c6de2102463aa0261ca7f4d6a216
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-05-04 13:40:48 +0600

    add: game config controls

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+import { GameConfig } from './snake-game/snake-game';
 import { Game } from './snake-game'
 import './index.css'
 
-const canvas = <HTMLCanvasElement>document.getElementById('canvas')
-const game = new Game(canvas, {
+let config: GameConfig = {
   width: 15,
   height: 20,
   partSize: 20,
@@ -10,6 +10,34 @@ const game = new Game(canvas, {
   gameSpeed: 300,
   grid: false,
   wallCollision: false
+}
+
+const form = <HTMLFormElement>document.getElementById('config')
+const canvas = <HTMLCanvasElement>document.getElementById('canvas')
+let game = new Game(canvas, { ...config })
+
+const toInteger = (str: any): number => {
+  return parseInt(str)
+}
+
+form.addEventListener('change', () => {
+  const formData = new FormData(form)
+
+  config['width'] = toInteger(formData.get('width'))
+  config['height'] = toInteger(formData.get('height'))
+  config['partSize'] = toInteger(formData.get('partSize'))
+  config['snakeLength'] = toInteger(formData.get('snakeLength'))
+  config['gameSpeed'] = toInteger(formData.get('gameSpeed'))
+  config['grid'] = formData.get('grid') ? true : false
+  config['wallCollision'] = formData.get('wallCollision') ? true : false
 })
 
-game.start()
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  game.stop()
+
+  game = new Game(canvas, { ...config })
+
+  game.start()
+})
